76da90f044d01da096a51d2df940d133bed42648
unreal <benwhite2413@gmail.com>
2022-04-15 20:20:37 +1200

    configpatch

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { env } from "process";
 
 const config = {
-    webSocketUrl: env.RT_URL ? env.RT_URL : "INVALID_URL",
+    webSocketUrl: env.RT_URL ? env.RT_URL : "ws://localhost:8080",
 };
 
 export default config;
