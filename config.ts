20e546e9e0c848452ad72adc1b99fb9b56ed67b7
unreal <benwhite2413@gmail.com>
2022-04-16 12:39:55 +1200

    Change URL

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { env } from "process";
 
 const config = {
-    webSocketUrl: env.RT_URL ? env.RT_URL : "ws://localhost:8080",
+    webSocketUrl: env.RT_URL ? env.RT_URL : "ws://localhost:8000",
 };
 
 export default config;
