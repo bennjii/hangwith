329acdec7e9f72bfa97da1afd863169716867d66
UnRealReincarlution <benwhite2413@gmail.com>
2022-04-22 09:48:30 +1200

    Changed STUN Servers

--- public/src/hang_client.ts
+++ public/src/hang_client.ts
@@ -102,24 +102,16 @@ export type HangClientProps = {
 export const default_config: Partial<RTCConfiguration> = {
     iceServers: [
         {
-          urls: "stun:openrelay.metered.ca:80"
+            urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
+            ]
         },
-        {
-          urls: "turn:openrelay.metered.ca:80",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443?transport=tcp",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        }
-    ]
+    ],
+    iceCandidatePoolSize: 10,
 };
 
 const default_constraints = {
