1501fb07a765e4639e83a30bf0e3769468ad532f
Ben White <37007079+bennjii@users.noreply.github.com>
2022-09-26 16:04:06 +1300

    Changing websocket URL

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { env } from "process";
 
 const config = {
-    webSocketUrl: "wss://128.199.234.165:8000",
-    // webSocketUrl: "ws://localhost:8000",
+    // webSocketUrl: "wss://128.199.234.165:8000",
+    webSocketUrl: "wss://rts.bennjii.dev:8000",
 };
 
 export default config;
