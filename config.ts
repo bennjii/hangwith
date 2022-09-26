1e9114abab740d4fd5280293e2beedb3f1cf4294
Ben White <37007079+bennjii@users.noreply.github.com>
2022-09-26 16:09:30 +1300

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { env } from "process";
 
 const config = {
     // webSocketUrl: "wss://128.199.234.165:8000",
-    webSocketUrl: "wss://rts.bennjii.dev:8000",
+    webSocketUrl: "wss://rts.bennjii.dev",
 };
 
 export default config;
